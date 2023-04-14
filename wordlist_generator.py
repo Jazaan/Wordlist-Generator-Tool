c3c24b0f19c8c95483e119fd277bbacd5a83e64b
Abbas Jazaan J <74532284+Jazaan@users.noreply.github.com>
2023-04-14 21:26:53 +0530

    Update wordlist_generator.py

--- wordlist_generator.py
+++ wordlist_generator.py
@@ -1,59 +1,45 @@
 import itertools
 import string
 
-def create_wordlist(length, chars, format):
+def create_wordlist(length, chars):
     """
-    Generate a wordlist based on the specified length, characters, and format.
+    Generate a wordlist based on the specified length and characters.
 
     Args:
         length (int): The length of each word in the wordlist.
         chars (str): The characters to include in the wordlist.
-        format (str): The format of each word in the wordlist, where
-                      'L' represents a lowercase letter, 'U' represents
-                      an uppercase letter, 'D' represents a digit, and
-                      'S' represents a symbol.
 
     Returns:
         A list of all possible combinations of the specified characters
-        with the specified length and format.
+        with the specified length.
     """
     wordlist = []
     for combination in itertools.product(chars, repeat=length):
-        word = ""
-        for i, char_type in enumerate(format):
-            if char_type == "L":
-                word += combination[i].lower()
-            elif char_type == "U":
-                word += combination[i].upper()
-            elif char_type == "D":
-                word += str(combination[i])
-            elif char_type == "S":
-                word += combination[i]
+        word = "".join(combination)
         wordlist.append(word)
     return wordlist
 
 # User inputs
 length = int(input("Enter the length of each word in the wordlist: "))
-include_numbers = input("Include numbers in the wordlist? (y/n): ")
-include_uppercase = input("Include uppercase letters in the wordlist? (y/n): ")
-include_symbols = input("Include symbols in the wordlist? (y/n): ")
-format = input("Enter the format of each word in the wordlist (e.g. LLDDSS): ")
-output_file = input("Enter the filename for the wordlist: ")
+include_numbers = True if input("Include numbers in the wordlist? (1/0): ") == "1" else False
+include_uppercase = True if input("Include uppercase letters in the wordlist? (1/0): ") == "1" else False
+include_symbols = True if input("Include symbols in the wordlist? (1/0): ") == "1" else False
+filename = raw_input("Enter the filename for the wordlist: ")
 
 # Define characters to include in the wordlist
 chars = string.ascii_lowercase
-if include_numbers.lower() == "y":
+if include_numbers:
     chars += string.digits
-if include_uppercase.lower() == "y":
+if include_uppercase:
     chars += string.ascii_uppercase
-if include_symbols.lower() == "y":
+if include_symbols:
     chars += string.punctuation
 
 # Generate the wordlist
-wordlist = create_wordlist(length, chars, format)
+wordlist = create_wordlist(length, chars)
 
 # Output the wordlist to a file
-with open(output_file, "w") as f:
+with open(filename, "w") as f:
     for word in wordlist:
         f.write(word + "\n")
 
